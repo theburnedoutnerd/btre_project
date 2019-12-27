f0d8bc9fb50c97147f2ed5b653d4f15bdb936228
Alissa McGee <comm.anyashepard@gmail.com>
2019-12-26 19:45:29 -0600

    Pull Data From Listings Model

--- listings/views.py
+++ listings/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+from .models import Listing
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'listings/listings.html')
+    listings = Listing.objects.all()
+
+    context = {
+        'listings' : listings
+    }
+    
+    return render(request, 'listings/listings.html', context)
 
 def listing(request):
     return render(request, 'listings/listing.html')
