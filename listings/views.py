ae50ac2113ec788531e47a46dc35e990718d8dce
Alissa McGee <comm.anyashepard@gmail.com>
2020-01-13 19:53:43 -0600

    Created Pagination for Listings View

--- listings/views.py
+++ listings/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 from .models import Listing
 
@@ -7,8 +8,12 @@ from .models import Listing
 def index(request):
     listings = Listing.objects.all()
 
+    paginator = Paginator(listings, 3)
+    page = request.GET.get('page')
+    page_listings = paginator.get_page(page)
+
     context = {
-        'listings' : listings
+        'listings' : page_listings
     }
     
     return render(request, 'listings/listings.html', context)
